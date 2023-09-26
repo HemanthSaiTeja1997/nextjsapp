d1a0f8c42bef96eb999c50d36d4a57ac596fe0b5
HemanthSaiTeja9652 <srinivaspulusuganti@gmail.com>
2023-09-26 17:35:41 +0530

    commit

--- src/app/employee/page.tsx
+++ src/app/employee/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 const Employee = () => {
   return (
     <div>
       <h1 className='m-1 p-1 flex flex-col items-center justify-center border rounded-lg bg-slate-500 '>Hi Welcome Back User </h1>
+   <Link href='/login'>VISITLOGIN</Link>
     </div>
   )
 }
